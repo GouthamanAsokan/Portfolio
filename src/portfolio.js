4734cdb97b510facf742f1b9bdf9064bdaa68d2a
Gouthaman-sigmoid <gouthaman@sigmoidanalytics.com>
2025-06-22 22:09:34 +0530

    portfolio commit

--- src/portfolio.js
+++ src/portfolio.js
@@ -714,7 +714,7 @@ const contactPageData = {
     title: "Contact Me",
     profile_image_path: "profile.jpg",
     description:
-      "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.",
+      "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML and DL Development.",
   },
   blogSection: {
     title: "Blogs",
